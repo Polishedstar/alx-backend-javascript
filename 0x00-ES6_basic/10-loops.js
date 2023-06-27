97785fd79e86759bec3ac29965ca942bbd501192
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2023-06-27 05:37:50 -0700

    0x00-ES6_basic files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
